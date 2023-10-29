ce2c60436ee92a08260f95ab64d965b9d73433cb
Tim <92823021+coldhandsprod@users.noreply.github.com>
2023-10-29 16:38:48 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var facts = {
+  5: 'There are approximately 5 liters of blood in an average adult human body.',
+  10: 'On average, a person might use about 10 liters of water for a single shower.',
+  20: 'A typical washing machine uses around 20 liters of water for one load of laundry.',
+  50: '50 liters of grape juice can yield about 67 standard bottles of wine.',
+  80: '80 liters of liquid chocolate can produce over 640 standard chocolate bars.',
+  302: 'A standard bathtub will hold roughly around 80 gallons (302 liters) of water.'
+};
+
 let extra_amount = 0;
 
 function add_bottle(amount) {
@@ -30,5 +39,13 @@ function add_bottle(amount) {
     img.src = "https://www.freeiconspng.com/thumbs/water-bottle-png/drinking-water-bottle-png-9.png";
     img.width = 60;
     document.body.appendChild(img);
+  }
+
+  var volume = Math.trunc(value);
+  
+  if (facts.includes(volume)) {
+    // Use facts[volume] to display an interesting fact.
+    console.log(facts[volume])
+    // THERE WAS AN ERROR SO CHECK IT OUT AND FIX IT
   }
 }
