4c51aa3dce1750b50436d7faff8cb615c2c07613
xine91 <148487472+xine91@users.noreply.github.com>
2023-11-07 20:53:05 +0100

    Update cookie-form.php

--- cookie-form.php
+++ cookie-form.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $cookie_name = "user";
 
 if(!isset($_COOKIE[$cookie_name])) {
@@ -7,7 +9,27 @@ if(!isset($_COOKIE[$cookie_name])) {
 } else {
   // echo "Cookie '" . $cookie_name . "' is set!<br>";
   // echo "Value is: " . $_COOKIE[$cookie_name];
-  header('location:profile2.html');
-}
+  $host = "localhost";
+  $dbname = "kb_info_db";
+  $username = "root";
+  $password = "";
 
-?>  
+  $conn = mysqli_connect(hostname: $host,
+                        username: $username,
+                        password: $password,
+                        database: $dbname);
+
+  if (mysqli_connect_errno()) {
+    die("Connection error: " . mysqli_connect_error());
+  }
+  
+  $query_name=" SELECT * FROM registration WHERE email = '$_COOKIE[$cookie_name]' ";
+  
+  $result=mysqli_query($conn, $query_name); 
+  $row = mysqli_fetch_array($result);
+  $test = implode($row);
+  $_SESSION['test'] = $test;
+
+  header("location:profile2.php?test=" . urlencode($test));
+}
+?>      
