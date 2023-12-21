4039e01d3648064f51c21ffb66ab6848994b71a9
ZyreXInfinity <78761495+ZyreXINF@users.noreply.github.com>
2023-12-21 18:44:33 +0200

    Update cookie-form.php

--- cookie-form.php
+++ cookie-form.php
@@ -10,15 +10,12 @@ if(!isset($_COOKIE[$cookie_name])) {
   // echo "Cookie '" . $cookie_name . "' is set!<br>";
   // echo "Value is: " . $_COOKIE[$cookie_name];
 
-  $host = "localhost";
-  $dbname = "kb_info_db";
-  $username = "root";
-  $password = "";
-
-  $conn = mysqli_connect(hostname: $host,
-                        username: $username,
-                        password: $password,
-                        database: $dbname);
+  $host = "localhost:3306";
+  $dbname = "pjbelamy_ks_data";
+  $username = "pjbelamy_ks_admin";
+  $password = "-}3l3fg0t^ZCai!wT]";
+  
+  $conn = mysqli_connect($host, $username, $password, $dbname);
 
   if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_error());
