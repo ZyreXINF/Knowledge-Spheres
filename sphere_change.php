3c97379e7ca3be1a90829cc61cbcfb361a6030f1
ZyreXInfinity <78761495+ZyreXINF@users.noreply.github.com>
2023-12-21 18:49:37 +0200

    Update sphere_change.php

--- sphere_change.php
+++ sphere_change.php
@@ -7,33 +7,4 @@ if (isset($_GET['spherename'])) {
     echo 'Error: IT AINT WORKING';
 }
 
-// $host = "localhost";
-// $dbname = "kb_info_db";
-// $username = "root";
-// $password = "";
-
-// $conn = mysqli_connect(hostname: $host,
-//                        username: $username,
-//                        password: $password,
-//                        database: $dbname);
-
-// if (mysqli_connect_errno()) {
-//     die("Connection error: " . mysqli_connect_error());
-// }
-
-// $sql = "SELECT * FROM spheres WHERE name='$sphere_name' ";
-
-// $result = mysqli_query($conn, $sql);
-// $row = mysqli_fetch_array($result);
-
-// $sphere_name = $row[1];
-// $sphere_color = $row[2];
-// $sphere_volume = $row[3];
-// $sphere_tier = $row[4];
-
-// $_SESSION["sphere_name"] = $sphere_name;
-// $_SESSION["sphere_color"] = $sphere_color;
-// $_SESSION["sphere_volume"] = $sphere_volume;
-// $_SESSION["sphere_tier"] = $sphere_tier;
-
 header("location:mySphere2.php?sphere_name=" . urlencode($sphere_name));
